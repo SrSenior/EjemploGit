2c34561c834a82c7390db46a64534821878c73b6
jpveg <jpveg@Sr_Seor>
2023-10-20 12:43:13 -0600

    hola mundo a lo Flanders

--- src/main/java/com/mycompany/ejemplogit/EjemploGit.java
+++ src/main/java/com/mycompany/ejemplogit/EjemploGit.java
@@ -11,6 +11,6 @@ package com.mycompany.ejemplogit;
 public class EjemploGit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola mundirijillo");
     }
 }
