beebeac5e4ace947880eccbebf1ec6e4f0620172
jpveg <jpveg@Sr_Seor>
2023-10-25 15:12:41 -0600

    Sexo itermedio

--- src/main/java/com/mycompany/ejemplogit/EjemploGit.java
+++ src/main/java/com/mycompany/ejemplogit/EjemploGit.java
@@ -12,5 +12,6 @@ public class EjemploGit {
 
     public static void main(String[] args) {
         System.out.println("Hola mundirijillo");
+        ClaseDePrueba clsPr1 = new ClaseDePrueba();
     }
 }
